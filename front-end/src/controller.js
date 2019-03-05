15666c0e8c58c98776f12c909d2c0ebef64abc2a
mkay0961 <mkay0961@gmail.com>
2019-03-05 10:54:20 -0500

    skipping login

--- front-end/src/controller.js
+++ front-end/src/controller.js
@@ -6,10 +6,70 @@ class Controller{
   start(){
     console.log("starting");
     let promise = this.userAdapter.getAllUsers()
-    promise.then((userAray)=>{this.handleArray(userAray)})
+    // promise.then((userAray)=>{this.handleArray(userAray)})
+    promise.then((userAray)=>{this.specficUser(userAray[0])})
+    
 
   }
-
+  // checkLogin(){
+  //   //if logged in
+  //   //call call specficUser(user)
+  //   let userName = document.getElementById('userName');
+  //   let userPw = document.getElementById('userPw');
+  //
+  //   if((localStorage.getItem('name') !== userName.value)&& (localStorage.getItem('pw')!==userPw.value)){
+  //     console.log("not logged in");
+  //     this.loginPage()
+  //     debugger
+  //   }else if ((localStorage.getItem('name') === userName.value)&& (localStorage.getItem('pw')===userPw.value)){
+  //     console.log("logged in");
+  //     debugger
+  //   }
+  //   //else if not call loginpage()
+  //
+  // }
+  //
+  // loginPage(){
+  //   //have button to register registerPage()
+  //
+  //   this.getBody().innerText = ""
+  //   this.getBody().innerHTML = `<form id="login-form">
+  //           <input id="userName" type="text" placeholder="Enter Username" value=""/>
+  //           <input id="userPw" type="password" placeholder="Enter Password" value=""/>
+  //           <input id="login_btn" type="submit" value="Login"/>
+  //           <input id="register_btn" type="submit"value="Register"/>
+  //
+  //      </form>`
+  //
+  //      document.getElementById('register_btn').addEventListener('click', this.registerPage.bind(this))
+  //      document.getElementById('login_btn').addEventListener('click',this.checkLogin.bind(this))
+  // }
+  //
+  //
+  //
+  // registerPage(){
+  //   this.getBody().innerText = ""
+  //   this.getBody().innerHTML = `<form id="register-form">
+  //           <input id="name" type="text" placeholder="Name" value=""/>
+  //           <input id="pw" type="password" placeholder="Password" value=""/>
+  //           <input id="rgstr_btn" type="submit" value="Create Account"/>
+  //       </form>`
+  //
+  //   document.getElementById('register-form').addEventListener('submit',(e)=>{ this.newAccount(e)})
+  //
+  // }
+  //
+  // newAccount(e){
+  //   e.preventDefault()
+  //   let name = document.getElementById('name');
+  //   let pw = document.getElementById('pw');
+  //
+  //   localStorage.setItem('name', name.value);
+  //   localStorage.setItem('pw', pw.value);
+  //
+  //   this.loginPage()
+  //   debugger
+  // }
   handleArray(array){
     this.getBody().innerText = ""
     array.forEach(this.renderUser.bind(this))
@@ -44,12 +104,8 @@ class Controller{
     let h1 = document.createElement("h1")
     h1.innerText = user.first + " " + user.last
 
-    let btn = document.createElement("button")
-    btn.innerText = "Home"
-    btn.addEventListener('click',()=>{this.start()})
     let div = this.createUserDiv()
     div.appendChild(h1)
-    div.appendChild(btn)
     this.getBody().appendChild(div)
 
     console.log("hi");
